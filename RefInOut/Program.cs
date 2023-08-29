c2d71aab80f8c8fdeee5448319679960619ef147
SnnBrgmv <sananai@code.edu.az>
2023-08-29 17:20:47 +0400

    Custom TryParse method with out keyword

---
+++ RefInOut/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace RefInOut
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Input string:");
+            string value = Console.ReadLine();
+
+            if(CustomTryParse(value,out int result))
+            {
+                Console.WriteLine("Parsed successfully: " + result);
+            }
+            else
+            {
+                Console.WriteLine("Parsed failed: " + result);
+            }
+            
+        }
+        public static bool CustomTryParse(string value,out int result)
+        {
+            result = 0;
+            try
+            {
+                result = int.Parse(value);
+                return true;
+            }
+            catch 
+            { 
+                return false; 
+            }
+        }
+    }
+}
